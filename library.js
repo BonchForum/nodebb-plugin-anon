39324beb0d5535b68e26e7826b4a11da94b941a4
luchanso <oleg@loutchansky.com>
2016-01-24 18:28:49 +0300

    Create carcase

--- library.js
+++ library.js
@@ -1,6 +1,46 @@
 (function(module) {
   "use strict";
-  var anon = { };
+  var anon = {},
+    ssettings = {},
+    winston = module.parent.require('winston');
+
+  anon.init = function(params, callback) {
+    var router = params.router,
+      hostMiddleware = params.middleware;
+
+    router.get('/admin/plugins/anon', hostMiddleware.admin.buildHeader, render);
+    router.get('/api/admin/plugins/anon', render);
+
+    callback();
+  }
+
+  anon.filter = function(data, callback) {
+    winston.info('[nodebb-plugin-anon] ' + data);
+
+    callback(data);
+  }
+
+  anon.addAdminNavigation = function(header, callback) {
+    header.plugins.push({
+      route: '/plugins/anon',
+      icon: 'fa-tint',
+      name: 'Anon'
+    });
+
+    callback(null, header);
+  }
+
+  function render(req, res) {
+    res.render('admin/plugins/anon', {});
+  }
+
+  function saveSettings() {
+
+  }
+
+  function loadSettings() {
+    
+  }
 
   module.exports = anon;
 }(module));
